6497bb746274654045789276d34b4aa5051b4ba3
AliaksandraPashko <sasha_pashko@mail.ru>
2018-02-28 00:36:33 +0300

    one dropdownlist

---
+++ Lab1(regions)/MainForm.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="Lab1_regions_.MainForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+           
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ Lab1(regions)/MainForm.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Lab1_regions_
+{
+    public partial class MainForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["CountryDBConnectionString"].ConnectionString;
+        
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("SELECT ID, Name FROM Regions", connection);
+            connection.Open();
+            DropDownList1.DataSource = cmd.ExecuteReader();
+            DropDownList1.DataTextField = "Name";
+            DropDownList1.DataValueField = "ID";
+            DropDownList1.DataBind();
+            connection.Close();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
